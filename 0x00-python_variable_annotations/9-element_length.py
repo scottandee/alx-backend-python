2c15f4d7827f3c6208b39630865b03d3a0d279c2
Andee Vagrant Vscode <andeeolayinkascott@gmail.com>
2023-12-07 16:25:42 +0000

    Task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This script aims at practicing duck typing"""
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
