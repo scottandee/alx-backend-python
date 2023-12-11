6a29b7b1b03e78c9afe24971721916b04f1fbb7a
Andee Vagrant Vscode <andeeolayinkascott@gmail.com>
2023-12-11 19:41:44 +0000

    Task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This script contains basic async syntax"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10):
+    """This function waits for a random delay
+    between 0 and `max_delay` seconds and eventually
+    returns it
+    """
+    n = random.uniform(0, max_delay)
+    await asyncio.sleep(n)
+    return n
