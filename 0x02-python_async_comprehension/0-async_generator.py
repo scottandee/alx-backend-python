2eb6d8e6eab09e9642e62e3fc1ea93850392eea9
Andee Vagrant Vscode <andeeolayinkascott@gmail.com>
2023-12-12 15:51:05 +0000

    Create async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This script contains the declaration of
+an asynchronous generator function
+"""
+
+import asyncio
+from typing import Generator
+import random
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """This is an asychronous generator function
+    that yields 10 values"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
