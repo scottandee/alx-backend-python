01ac4633fa25e0526650212de37e212b837c697b
Andee Vagrant Vscode <andeeolayinkascott@gmail.com>
2023-12-12 02:45:18 +0000

    Create an asyncio task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This script contains a function that creates
+an asyncio task
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """This function creates an asyncio task"""
+    return asyncio.create_task(wait_random(max_delay))
