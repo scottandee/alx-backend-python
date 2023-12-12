2fe7779aae6a3487134284c0ef260b1651cdde15
Andee Vagrant Vscode <andeeolayinkascott@gmail.com>
2023-12-12 02:57:19 +0000

    Task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This script contains a function that
+executes multiple coroutines
+"""
+
+from typing import List, Callable
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """This function spawns `task_wait_random` `n` times
+    with the specified maximum delay
+    """
+    delays: List[float] = await asyncio.gather(
+        *(task_wait_random(max_delay) for i in range(n))
+    )
+    return delays
