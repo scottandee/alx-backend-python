0a09fb89fc8f1bc9c6b11042a7485cb2898cc6c5
Andee Vagrant Vscode <andeeolayinkascott@gmail.com>
2023-12-12 02:17:09 +0000

    Concurrent coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This script contains a function that
+executes multiple coroutines
+"""
+
+from typing import List, Callable
+import asyncio
+import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """This function spawns `wait_random` `n` times
+    with the specified maximum delay
+    """
+    delays: List[float] = await asyncio.gather(
+        *(wait_random(max_delay) for i in range(n))
+    )
+    return delays
