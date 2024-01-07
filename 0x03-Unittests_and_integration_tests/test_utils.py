3fb3371535704a527117a11e0b39babb650fd8a0
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-07 06:57:56 +0100

    Fix shebang

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env pypython3
+#!/usr/bin/env python3
 """Test Utils
 """
 
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
-from typing import Any, Dict, Set
+from typing import Any, Dict, Set, Callable
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,8 +15,10 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: Dict, path: Set, expected_output: Any) -> None:
-        """Test the access nested map function"""
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """This method tests the access nested
+        map function
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
