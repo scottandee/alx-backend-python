499bf7201bbff833fe2eb250cb1c9146cd0ebbee
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-07 07:07:56 +0100

    Pycodestyle fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,10 +22,10 @@ class TestAccessNestedMap(unittest.TestCase):
         map function
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
-    
+
     @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1},("a", "b"))
+        ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """This tests the exception that is usually
