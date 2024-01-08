f097cd478cc2debfda1bc35ea76ffd1999d088ba
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-08 02:09:03 +0100

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 
 import unittest
 from unittest.mock import patch, MagicMock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 
 
@@ -49,3 +49,28 @@ class TestGetJson(unittest.TestCase):
         mock_response.json.return_value = test_payload
         mock_requests.return_value = mock_response
         self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """This class contains tests for the momoize
+    function
+    """
+    def test_memoize(self):
+        """This function tests the memoize function"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        instance = TestClass()
+        with patch.object(instance, "a_method") as mocked_a_method:
+            mocked_a_method.return_value = 42
+
+            result_1 = instance.a_property
+            result_2 = instance.a_property
+
+            mocked_a_method.assert_called_once()
+            self.assertEqual(result_1, result_2)
