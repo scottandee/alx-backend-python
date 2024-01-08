eb433f440869ead3ab70ed05e107db7f37f9cc71
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-08 03:45:15 +0100

    Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 
@@ -21,3 +21,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = {"test_key": test_org_name}
         client = GithubOrgClient(test_org_name)
         self.assertEqual(client.org, {"test_key": test_org_name})
+
+    def test_public_repos_url(self):
+        """Test that the result of _public_repos_url
+        is the expected one
+        """
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mock_org:
+            org_name = "google"
+            url = f"https://www.{org_name}.com"
+            mock_org.return_value = {"repos_url": url}
+            client = GithubOrgClient("google")
+            self.assertEqual(client._public_repos_url, url)
