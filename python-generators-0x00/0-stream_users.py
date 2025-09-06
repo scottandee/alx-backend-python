96e0c9600d9fdb2e06a56fbe4b02ac7b41943b37
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-09-06 09:59:12 +0100

    Create function to stream users from db one at a time

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+connect_to_prodev =  __import__('seed').connect_to_prodev
+
+def stream_users():
+    """
+    This function uses a generator to fetch rows,
+    one-by-one, from the user_data table
+    """
+    connection = connect_to_prodev()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        yield row
+
