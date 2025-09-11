c00a3a448e2e1fc37b1bc9da20bc4129a6fc9e2d
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-09-11 17:24:06 +0100

    Create class based context manager to handle database connection

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sqlite3
+
+
+class DatabaseConnection:
+    def __enter__(self):
+        """
+        Setup method:
+        Creates a new SQLite database connection
+        when used in a context manager.
+        """
+        self.conn = sqlite3.connect("users.db")
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Teardown method:
+        - Commits if no exception occurred
+        - Rolls back if an error occurred
+        - Always closes the connection
+        """
+        if hasattr(self, "conn"):
+            if exc_type is None:
+                self.conn.commit()
+            else:
+                self.conn.rollback()
+            self.conn.close()
+
+
+if __name__ == "__main__":
+    with DatabaseConnection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+        print(users)
