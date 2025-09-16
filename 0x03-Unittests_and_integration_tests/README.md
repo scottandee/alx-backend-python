aa2a6d8ce579a315ca9c78889bfc7f92eb53b8e6
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-09-16 08:57:26 +0100

    Remove old solution

--- 0x03-Unittests_and_integration_tests/README.md
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -1,1 +1,1 @@
-# 0x03: Unittests and Integration Tests
+# 0x03 - Unit and Integration Tests
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -73,4 +73,3 @@ def memoize(fn: Callable) -> Callable:
         return getattr(self, attr_name)
 
     return property(memoized)
-
