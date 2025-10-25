44d8fa6a3ee026b9fa0b895946b1722f04b2bb17
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-10-25 22:30:17 +0100

    configure middleware in settings

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware'
+    'chats.middleware.RestrictAccessByTimeMiddleware'
 ]
 
 LOGGING = {
