2f836c70d8f60a8501f5bcadcb3ceec754082654
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-10-25 22:28:21 +0100

    Add middleware to restrict chat by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import datetime, time
 import logging
+from django.utils import timezone
+from rest_framework import status
+from django.http import JsonResponse
 
 class RequestLoggingMiddleware:
     """
@@ -16,3 +19,26 @@ class RequestLoggingMiddleware:
         response = self.get_response(request)
 
         return response
+
+class RestrictAccessByTimeMiddleware:
+    """
+    This middleware restricts access to the chat
+    outside 9pm and 6pm
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        curr_time = timezone.now().time()
+        if not (curr_time > time(hour=18) and curr_time < time(hour=21)):
+            return JsonResponse(
+                {'error': 'Chat is restricted at this time'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        response = self.get_response(request)
+
+        return response
+
+        
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -68,7 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chats.middleware.RequestLoggingMiddleware'
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware'
 ]
 
 LOGGING = {
