1f40f246c75ec52144653a91b59fbe9a6eaf48bf
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-10-28 08:15:00 +0100

    Add middleware configurations

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -70,6 +70,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware'
     'chats.middleware.RestrictAccessByTimeMiddleware'
+    'chats.middleware.OffensiveLanguageMiddleware'
+    'chats.middleware.RolepermissionMiddleware'
 ]
 
 LOGGING = {
